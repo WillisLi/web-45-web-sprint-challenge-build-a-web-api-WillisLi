e814ea8a46290e4344787857db09a026664fe7e2
WillisLi <li.willis95@gmail.com>
2021-09-02 15:04:29 -0700

    set up server.js and routers

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,16 @@
-// Write your "actions" router here!
+const express = require('express')
+const Actions = require('./actions-model');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(actions => {
+            if (!actions) {
+                res.status(200).json([])
+            } else {
+                res.status(200).json(actions)
+            }
+        })
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -15,8 +15,8 @@ Pull your server into this file and start it!
 
 const server = require('./api/server');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-    console.log('listening on port 5000')
+    console.log(`listening on port ${PORT}`)
 })
